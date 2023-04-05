3f080e194c25496835a5d706dabc7dcf8fb9738b
hackpres <102390515+hackpres@users.noreply.github.com>
2023-04-05 10:21:43 -0400

    using the success of added THREE mesh textures I went back to the imported materials and added THREE.Colors to come closer to my desired look. git push

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,18 @@ import { Model as WallAnimation } from './components/3d/cube/WallAnimation';
 import { Suspense } from 'react';
 // import { OrbitControls } from '@react-three/drei';
 // import Cylinder3d from './components/3d/Cylinder3d';
-
+console.log(window.innerWidth)
 function App() {
   return (
     <>
       {/* <TriDCube /> */}
-      <section className="App-header">
+      <section className="App-header" width={window.innerWidth} >
         {/* Canvas 1 */}
         <Canvas
           camera={{ position: [0, 4,50], fov: 7.85, rotation: [-.025, -0.09, 0.006] }}
         >
-          {<pointLight position={[10, 10, 10]} intensity={.4} />}
-          {<ambientLight color={'#404040'} intensity={.8} />}
+          {<pointLight position={[10, 10, 10]} intensity={1} />}
+          {<ambientLight color={'#404040'} intensity={2} />}
           <Suspense fallback={null}>
             <WallAnimation />
           </Suspense>
--- client/src/components/3d/cube/WallAnimation.jsx
+++ client/src/components/3d/cube/WallAnimation.jsx
@@ -7,14 +7,14 @@ import React, { useEffect, useRef } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei';
 import * as THREE from 'three';
 
-const backWall = new THREE.MeshPhongMaterial();
-backWall.color = new THREE.Color('#21353d');
-const impactWall = new THREE.MeshLambertMaterial();
-impactWall.color = new THREE.Color('#3ca4af');
-const objectMat = new THREE.MeshPhongMaterial();
-objectMat.color = new THREE.Color('#f6eed7');
-const floorPlane = new THREE.MeshBasicMaterial();
-floorPlane.color = new THREE.Color('#0f262e');
+// const backWall = new THREE.MeshPhongMaterial();
+// // materials['backWall'].color = new THREE.Color('#21353d');
+// const impactWall = new THREE.MeshLambertMaterial();
+// impactWall.color = new THREE.Color('#3ca4af');
+// const objectMat = new THREE.MeshPhongMaterial();
+// objectMat.color = new THREE.Color('#f6eed7');
+// const floorPlane = new THREE.MeshBasicMaterial();
+// floorPlane.color = new THREE.Color('#0f262e');
 
 export function Model(props) {
   const group = useRef()
@@ -24,6 +24,10 @@ export function Model(props) {
 
   useEffect(() => {
     console.log(actions['cube1']);
+    materials['backWall'].color = new THREE.Color('#21353d');
+    materials['impactWall'].color = new THREE.Color('#3ca4af');
+    materials['floorPlane'].color = new THREE.Color('#0f262e');
+    materials['objectTexture'].color = new THREE.Color('#f6eed7');
     actions['cube1'].setLoop(THREE.LoopOnce);
     actions['cube2'].setLoop(THREE.LoopOnce);
     actions['cube3'].setLoop(THREE.LoopOnce);
@@ -54,18 +58,18 @@ export function Model(props) {
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
-        <mesh name="backWall" geometry={nodes.backWall.geometry} material={backWall} position={[0, 5.3, 0]} scale={[11.12, 5.37, 0.14]} />
-        <mesh name="floorPlane" geometry={nodes.floorPlane.geometry} material={floorPlane} position={[0, 0, 3.67]} scale={[11.15, 1, 3.88]} />
-        <mesh name="impactWall" geometry={nodes.impactWall.geometry} material={impactWall} position={[11.16, 5.3, 3.74]} rotation={[0, -Math.PI / 2, 0]} scale={[3.86, 5.37, 0.14]} />
-        <mesh name="bumper" geometry={nodes.bumper.geometry} material={floorPlane} position={[1.61, 0.02, 7.56]} rotation={[0, 0, -Math.PI]} scale={[-9.69, -0.07, -0.09]} />
-        <mesh name="cube1" geometry={nodes.cube1.geometry} material={objectMat} position={[-8.43, 5.92, 3.89]} scale={0.3} />
-        <mesh name="cube2" geometry={nodes.cube2.geometry} material={objectMat} position={[-9.41, 5.92, 5.25]} scale={0.3} />
-        <mesh name="cube3" geometry={nodes.cube3.geometry} material={objectMat} position={[-12.2, 5.25, 1.79]} rotation={[-0.67, 0.34, -0.15]} scale={0.3} />
-        <mesh name="cube4" geometry={nodes.cube4.geometry} material={objectMat} position={[-9.95, 3.7, 2.56]} rotation={[1.66, 0.6, 0.49]} scale={0.3} />
-        <mesh name="sphere1" geometry={nodes.sphere1.geometry} material={objectMat} position={[-11.64, 4.59, 4.03]} scale={0.36} />
-        <mesh name="sphere2" geometry={nodes.sphere2.geometry} material={objectMat} position={[-9.37, 3.11, 5.15]} scale={0.36} />
-        <mesh name="sphere3" geometry={nodes.sphere3.geometry} material={objectMat} position={[-15.03, 2.26, 2.88]} rotation={[0.51, -0.49, 0.29]} scale={0.36} />
-        <mesh name="sphere4" geometry={nodes.sphere4.geometry} material={objectMat} position={[-12.52, 3.2, 2.04]} rotation={[0.51, -0.49, 0.29]} scale={0.36} />
+        <mesh name="backWall" geometry={nodes.backWall.geometry} material={materials.backWall} position={[0, 5.3, 0]} scale={[11.12, 5.37, 0.14]} />
+        <mesh name="floorPlane" geometry={nodes.floorPlane.geometry} material={materials.floorPlane} position={[0, 0, 3.67]} scale={[11.15, 1, 3.88]} />
+        <mesh name="impactWall" geometry={nodes.impactWall.geometry} material={materials.impactWall} position={[11.16, 5.3, 3.74]} rotation={[0, -Math.PI / 2, 0]} scale={[3.86, 5.37, 0.14]} />
+        <mesh name="bumper" geometry={nodes.bumper.geometry} material={materials.floorPlane} position={[1.61, 0.02, 7.56]} rotation={[0, 0, -Math.PI]} scale={[-9.69, -0.07, -0.09]} />
+        <mesh name="cube1" geometry={nodes.cube1.geometry} material={materials.objectTexture} position={[-8.43, 5.92, 3.89]} scale={0.3} />
+        <mesh name="cube2" geometry={nodes.cube2.geometry} material={materials.objectTexture} position={[-9.41, 5.92, 5.25]} scale={0.3} />
+        <mesh name="cube3" geometry={nodes.cube3.geometry} material={materials.objectTexture} position={[-12.2, 5.25, 1.79]} rotation={[-0.67, 0.34, -0.15]} scale={0.3} />
+        <mesh name="cube4" geometry={nodes.cube4.geometry} material={materials.objectTexture} position={[-9.95, 3.7, 2.56]} rotation={[1.66, 0.6, 0.49]} scale={0.3} />
+        <mesh name="sphere1" geometry={nodes.sphere1.geometry} material={materials.objectTexture} position={[-11.64, 4.59, 4.03]} scale={0.36} />
+        <mesh name="sphere2" geometry={nodes.sphere2.geometry} material={materials.objectTexture} position={[-9.37, 3.11, 5.15]} scale={0.36} />
+        <mesh name="sphere3" geometry={nodes.sphere3.geometry} material={materials.objectTexture} position={[-15.03, 2.26, 2.88]} rotation={[0.51, -0.49, 0.29]} scale={0.36} />
+        <mesh name="sphere4" geometry={nodes.sphere4.geometry} material={materials.objectTexture} position={[-12.52, 3.2, 2.04]} rotation={[0.51, -0.49, 0.29]} scale={0.36} />
       </group>
     </group>
   )
